d3d51f05f00ab6567608b1d501ae501ec8b6c7ed
aviv <shaked.aviv1@gmail.com>
2015-04-18 22:14:19 +0300

    fix(validatorDirectives): Makes isIn a special case where array is not split into arguments

--- src/components/validatorDirectives.js
+++ src/components/validatorDirectives.js
@@ -6,28 +6,37 @@
 		.run([
 			'$validator',
 			'$validatorBuilder',
-			function ($validator, $validatorBuilder) {
-
-				function capitalizeFirstLetter(string) {
-					return string.charAt(0).toUpperCase() + string.slice(1);
-				}
+			'directiveNormalizer',
+			function ($validator, $validatorBuilder, directiveNormalizer) {
 
 				function buildValidatorsArray () {
 					var methodNames = ['equals', 'contains', 'matches', 'isEmail', 'isURL', 'isFQDN', 'isIP',
 						'isAlpha', 'isNumeric', 'isAlphanumeric', 'isBase64', 'isHexadecimal', 'isHexColor', 'isLowercase',
 						'isUppercase', 'isInt', 'isFloat', 'isDivisibleBy', 'isNull', 'isLength', 'isByteLength', 'isUUID',
-						'isDate', 'isAfter', 'isBefore', 'isIn', 'isCreditCard', 'isISIN', 'isISBN', 'isMobilePhone', 'isJSON',
+						'isDate', 'isAfter', 'isBefore', 'isCreditCard', 'isISIN', 'isISBN', 'isMobilePhone', 'isJSON',
 						'isMultibyte', 'isAscii', 'isFullWidth', 'isHalfWidth', 'isVariableWidth', 'isSurrogatePair', 'isMongoId',
 						'isCurrency'];
 					return methodNames.map(function (methodName) {
 						return {
-							directiveName: 'ngv' + capitalizeFirstLetter(methodName),
-							validatorName: methodName,
+							directiveName: directiveNormalizer.convertMethodNameToDirectiveName(methodName, 'ngv'),
+							validatorName: directiveNormalizer.convertMethodNameToDirectiveName(methodName),
 							validator: $validator[methodName]
 						};
 					});
 				}
 				$validatorBuilder.buildValidators(buildValidatorsArray());
+
+				// isIn is a special case because it needs to get an array as a single argument
+				$validatorBuilder.buildValidator({
+					directiveName: 'ngvIsIn',
+					validatorName: 'isIn',
+					validator: function () {
+						debugger;
+						var args = Array.prototype.slice.call(arguments);
+						var val = args.shift();
+						return $validator.isIn(val, args);
+					}
+				});
 			}
 		]);
 }());
